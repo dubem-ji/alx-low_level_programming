ecc5a1c4d8b078e7b46314cfd951946dd38146b2
Dubem <mapt.dubem@gmail.com>
2023-09-11 03:14:19 -0700

    Fix bug in 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,6 +5,7 @@
  * main - Entry point
  *
  * Return: Always 0
+ *
  */
 
 int main(void)
@@ -13,15 +14,15 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n < 0) 
+	if (n < 0) 
 	{
-		printf("%d is negative", n);
-	} else if(n == 0)
+		printf("%d is negative\n", n);
+	} else if (n == 0)
 	{
-		printf("%d is zero", n);
+		printf("%d is zero\n", n);
 	} else 
 	{
-		printf("%d id positive", n);
+		printf("%d id positive\n", n);
 	}
 	return (0);
 }
